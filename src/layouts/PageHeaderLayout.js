7ddb3bb5167bba86b4db2c3d6fe5c27839d96545
zhengzhuanzhuan <zhengzhuanzhuan@startdt.com>
2018-09-04 11:25:21 +0800

    updata

---
+++ src/layouts/PageHeaderLayout.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FormattedMessage } from 'umi/locale';
+import Link from 'umi/link';
+import PageHeader from '@/components/PageHeader';
+import GridContent from './GridContent';
+import styles from './PageHeaderLayout.less';
+import MenuContext from './MenuContext';
+import { connect } from 'dva';
+
+const PageHeaderLayout = ({ children, grid, wrapperClassName, top, ...restProps }) => (
+  <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
+    {top}
+    <MenuContext.Consumer>
+      {value => {
+        return (
+          <PageHeader
+            wide={grid === 'Wide'}
+            home={<FormattedMessage id="menu.home" defaultMessage="Home" />}
+            {...value}
+            key="pageheader"
+            {...restProps}
+            linkElement={Link}
+            itemRender={item => {
+              if (item.locale) {
+                return <FormattedMessage id={item.locale} defaultMessage={item.name} />;
+              }
+              return item.name;
+            }}
+          />
+        );
+      }}
+    </MenuContext.Consumer>
+    {children ? (
+      <div className={styles.content}>
+        <GridContent>{children}</GridContent>
+      </div>
+    ) : null}
+  </div>
+);
+
+export default connect(({ setting }) => ({
+  grid: setting.grid,
+}))(PageHeaderLayout);
